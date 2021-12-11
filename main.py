49852ba4f02a80e87c409218dee6f7299046a77f
Jiseong Heo <jsheo@postech.ac.kr>
2021-12-11 14:42:51 +0900

    time/frame: 0.258

--- main.py
+++ main.py
@@ -3,47 +3,39 @@
 from pose_estimation.utils.vis import vis_3d_multiple_skeleton
 import numpy as np
 from pose_estimation.config import cfg
-from pose_estimation.pose_estimator import PoseEstimator
+from pose_estimation.pose_estimator import PoseEstimatorV2
 import os
 import cv2
 import sys
 sys.path.insert(0, 'human_detection')
 
 from human_detection.detector import DetectNet
-
+import torch
+import time
 if __name__ == '__main__':
 
     human_detector = DetectNet()
-    pose_estimator = PoseEstimator()
+    pose_estimator = PoseEstimatorV2()
     data_folder = '/Data/3D_pose_estimation_dataset/PIROPO/Room A/omni_1A/omni1A_test6'
     data_folder = '/Data/3D_pose_estimation_dataset/CEPDOF/Edge_cases'
     # data_folder = '/Data/3D_pose_estimation_dataset/MuPoTS/data/MultiPersonTestSet/TS20'
     # data_folder = '/Data/3D_pose_estimation_dataset/MuCo/data/unaugmented_set/1'
     # data_folder = '/Data/3D_pose_estimation_dataset/MuPoTS/data/MultiPersonTestSet/TS1'
     # data_folder = '/Data/3D_pose_estimation_dataset/RAPID'
-    # save_folder = '/Data/3D_pose_estimation_dataset/demo_result'
     if __name__ == '__main__':
         for fn in sorted(os.listdir(data_folder)):
             if 'jpg' not in fn and 'png' not in fn:
                 continue
+            start = time.time()
             path = os.path.join(data_folder, fn)
             frame = cv2.imread(path)
-
             patches, k_values = human_detector.detect(frame)
-            for patch in patches:
-                cv2.imshow('patches[0]', patch.permute(1,2,0).cpu().numpy()[:,:,::-1])
-            # TODO feed those patches to pose estimator
-            # TODO get rid of bbox/keypoints transformation from pose_estimator
-            # frame = cv2.resize(frame, (256, 256))
-            poses, vis_kps = pose_estimator.forward(frame)
-            # pickle.dump((poses, vis_kps), open(os.path.join(save_folder, fn.split('.')[0]+'.pkl'), 'wb'))
-            for pose in poses:
+            for patch,k_value in zip(patches, k_values):
+                patch = patch.permute(1,2,0).cpu().numpy()
+                k_value = k_value.unsqueeze(0)
+                pose = pose_estimator.forward(patch, k_value)
                 # print(pose)
-                frame = pose_estimator.visualize(frame, pose)
-            # vis_3d_multiple_skeleton(vis_kps, np.ones_like(vis_kps), cfg.skeleton,
-            #                          'output_pose_3d (x,y,z: camera-centered. mm.)')
-            cv2.imshow('', cv2.resize(frame, None, fx=0.25, fy=0.25))
-            cv2.waitKey()
+            print('elapsed time to process: {}'.format(time.time()-start))
 
 
 
